4e117b61fa3e8ec44537f9e9f9c8fc04acc9ca71
Kevin McQuown <kevinmcquown@me.com>
2022-07-24 11:17:42 -0500

    can now modify advertising uuid in lower byte from 0 - 9

--- src/BluetoothAdvertiser.ino
+++ src/BluetoothAdvertiser.ino
@@ -16,10 +16,14 @@ SYSTEM_THREAD(ENABLED);
 #define kWaitingTimerBetweenAdvertisements 30000
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const BleUuid serviceUuid("b025071e-09df-418a-beff-f64aea621700");
+uint8_t packetNumber;
+String temp = String("b025071e-09df-418a-beff-f64aea62170");
+String serviceUUIDString = temp + String(EEPROM.read(0x00));
+
+const BleUuid serviceUuid(serviceUUIDString.c_str());
 
 BleCharacteristic insBatteryLevelCharacteristicUuid("insBatteryLevel", BleCharacteristicProperty::NOTIFY | BleCharacteristicProperty::READ, BleUuid("b026072e-09df-418a-beff-f64aea621766"), serviceUuid);
 BleCharacteristic insConnectionCharacteristicUuid("insConnection", BleCharacteristicProperty::NOTIFY | BleCharacteristicProperty::READ, BleUuid("B027073E-09DF-418A-BEFF-F64AEA621767"), serviceUuid);
@@ -31,8 +35,22 @@ BleCharacteristic seizureAlertCharacteristicUuid("seizureAlert", BleCharacterist
 
 BleAdvertisingData advData;
 
-enum states {waiting, startAdvertising, advertising, connected, disconnect, cancelAdvertising, setSeizureDetect};
-enum wifiStates {waitingToConnect, connectingToParticle, connectedToParticle};
+enum states
+{
+  waiting,
+  startAdvertising,
+  advertising,
+  connected,
+  disconnect,
+  cancelAdvertising,
+  setSeizureDetect
+};
+enum wifiStates
+{
+  waitingToConnect,
+  connectingToParticle,
+  connectedToParticle
+};
 
 states state;
 wifiStates wifiState;
@@ -53,12 +71,14 @@ bool waitingTimerTriggered = false;
 bool advertisingTimerTriggered = false;
 bool connectionTimerTriggered = false;
 
+bool updatingAdvertisingAddress = false;
 bool onWiFi = false;
 unsigned long waitForWiFiTimer = 5000 + millis();
 
 uint8_t buf[BLE_MAX_ADV_DATA_LEN];
 
-void connectCallback(const BlePeerDevice& peer, void* context){
+void connectCallback(const BlePeerDevice &peer, void *context)
+{
   state = connected;
   finalElapsedTimeToConnect = millis() - currentElapstedTimeToConnect;
   connectionCount++;
@@ -71,19 +91,24 @@ void connectCallback(const BlePeerDevice& peer, void* context){
   connectionTimer = millis() + 5000;
 }
 
-void disconnectCallback(const BlePeerDevice& peer, void* context){
+void disconnectCallback(const BlePeerDevice &peer, void *context)
+{
   state = waiting;
   connectionTimer = 0;
   advertisingTimer = 0;
   waitingTimer = millis() + (kWaitingTimerBetweenAdvertisements);
 }
 
-void setAdvertisingManufacturingData(bool alert) {
-  if (alert) {
+void setAdvertisingManufacturingData(bool alert)
+{
+  if (alert)
+  {
     buf[0] = 0xFF;
     buf[1] = 0xFF;
     buf[2] = useAdvertisingPacketForSeizureAlert ? 0x01 : 0x00;
-  } else {
+  }
+  else
+  {
     buf[0] = 0xFF;
     buf[1] = 0xFF;
     buf[2] = 0x00;
@@ -95,7 +120,7 @@ void configureBLE()
 {
   // BLE.on();
   BLE.setDeviceName("Tablet");
-  
+
   BLE.addCharacteristic(tabletBatteryLevelCharacteristicUuid);
   BLE.addCharacteristic(insBatteryLevelCharacteristicUuid);
   BLE.addCharacteristic(insConnectionCharacteristicUuid);
@@ -105,40 +130,56 @@ void configureBLE()
   BLE.addCharacteristic(seizureAlertCharacteristicUuid);
 
   // Advertise our private service only
-  advData.appendServiceUUID(serviceUuid);  
+  advData.appendServiceUUID(serviceUuid);
   setAdvertisingManufacturingData(false);
 }
 
 // setup() runs once, when the device is first turned on.
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  //while (!Serial.isConnected()) {}
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+  // while (!Serial.isConnected()) {}
+  // Serial.println(serviceUUIDString);
+  // while (true) {}
+  packetNumber = EEPROM.read(0x00);
+  if (packetNumber == 0xFF) {
+    packetNumber = 0;
+    EEPROM.put(0x00, packetNumber);
+  }
+  //  SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ; // Don't proceed, loop forever
   }
 
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
   pinMode(BUTTON_A, INPUT_PULLUP);
   pinMode(BUTTON_B, INPUT_PULLUP);
-  pinMode(BUTTON_C, INPUT_PULLUP);  
+  pinMode(BUTTON_C, INPUT_PULLUP);
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
   display.clearDisplay();
-  display.setCursor(0,0);
-  display.println("<- A Join WiFi");
-  display.println("");
+  display.setCursor(0, 0);
   display.println("ePad Sim v1.1");
+  display.println("<- A Join WiFi");
+  display.println("<- B Adv Packet");
   display.display();
-  while ((millis() < waitForWiFiTimer) && !onWiFi) {
-      if (!digitalRead(BUTTON_A)) {
-        onWiFi = true;
-        WiFi.on();
-        WiFi.connect();
-        wifiState = waitingToConnect;
-      }
+  while ((millis() < waitForWiFiTimer) && !(onWiFi || updatingAdvertisingAddress))
+  {
+    if (!digitalRead(BUTTON_A))
+    {
+      onWiFi = true;
+      WiFi.on();
+      WiFi.connect();
+      wifiState = waitingToConnect;
+    }
+    if (!digitalRead(BUTTON_B))
+    {
+      updatingAdvertisingAddress = true;
+    }
   }
 
   BLE.on();
@@ -158,206 +199,273 @@ float insBattery = 100;
 float tabletBattery = 100;
 float tabletDiskSpace = 100;
 
-void loop() {
+void loop()
+{
+  if (updatingAdvertisingAddress)
+  {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setCursor(0, 0);
+    display.println("A < +");
+    display.println("");
+    display.println("");
+    display.println("C < -");
+    display.setCursor(75, 13);
+    display.setTextSize(2);
+    display.print(packetNumber, HEX);
+    display.setTextSize(1);
+    display.display();
+    if (!digitalRead(BUTTON_A))
+    {
+      packetNumber++;
+      packetNumber = packetNumber % 10;
+      EEPROM.put(0x00, packetNumber);
+      delay(100);
+    }
+    if (!digitalRead(BUTTON_C))
+    {
+      if (packetNumber > 0) {
+        packetNumber--;
+        EEPROM.put(0x00, packetNumber);
+      }
+      delay(100);
+    }
+    return;
+  }
   // This mode is used to get on WiFi for OTA updates.
-  if (onWiFi) {
-        display.clearDisplay();
-        display.setCursor(0,0);
-        switch(wifiState) {
-          case waitingToConnect:
-            display.println("Joining WiFi...");
-            display.println("");
-            display.println("Antenna Connected?");
-            if (WiFi.ready()) {
-              wifiState = connectingToParticle;
-              Particle.connect();
-            }
-          break;
-          case connectingToParticle:
-            display.println("Finding Particle ...");
-            if (Particle.connected()) {
-              wifiState = connectedToParticle;
-            }
-          break;
-          case connectedToParticle:
-            display.println("Waiting for OTA ...");
-          break;
-        }
-        display.display();
-        return;
+  if (onWiFi)
+  {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.display();
+    switch (wifiState)
+    {
+    case waitingToConnect:
+      display.println("Joining WiFi...");
+      display.println("");
+      display.println("Antenna Connected?");
+      if (WiFi.ready())
+      {
+        wifiState = connectingToParticle;
+        Particle.connect();
+      }
+      break;
+    case connectingToParticle:
+      display.println("Finding Particle ...");
+      if (Particle.connected())
+      {
+        wifiState = connectedToParticle;
+      }
+      break;
+    case connectedToParticle:
+      display.println("Waiting for OTA ...");
+      break;
+    }
+    display.display();
+    return;
   }
   handleTimedEvents();
-  if (!digitalRead(BUTTON_A)) {
-    if (state == waiting) {
+  if (!digitalRead(BUTTON_A))
+  {
+    if (state == waiting)
+    {
       state = startAdvertising;
       currentElapstedTimeToConnect = millis();
       setAdvertisingManufacturingData(false);
     }
   }
 
-  if (!digitalRead(BUTTON_B)) {
+  if (!digitalRead(BUTTON_B))
+  {
     useAdvertisingPacketForSeizureAlert = !useAdvertisingPacketForSeizureAlert;
     delay(200);
   }
 
-  if (!digitalRead(BUTTON_C)) {
-    if (state == waiting) {
+  if (!digitalRead(BUTTON_C))
+  {
+    if (state == waiting)
+    {
       setSeizure();
       state = startAdvertising;
       currentElapstedTimeToConnect = millis();
       setAdvertisingManufacturingData(true);
     }
   }
-  switch(state) {
-    case waiting:
-        clearSeizure();
-        display.clearDisplay();
-        display.setCursor(0,0);
-        display.print("A <- Advertise    ");
-        display.print((waitingTimer - millis()) / 1000);
-        display.println("s");
-        if (useAdvertisingPacketForSeizureAlert) {
-          display.println("B <- Fast Mode: On");
-        } else {
-          display.println("B <- Fast Mode: Off");
-        }
-        display.println("C <- Seizure Detect!");
-        if (connectionCount > 0) {
-          // display.drawLine(0,24,128,24,1);
-          display.setCursor(0,25);
-          display.print(minTimeToConnect);
-          display.print(" - ");
-          display.print(totalFinalElapsedTimeToConnect / connectionCount);
-          display.print(" - ");
-          display.print(maxTimeToConnect);
-          display.print(" ");
-          display.println(connectionCount);
-        }
-        display.display();
-        if (waitingTimerTriggered) {
-          state = startAdvertising;
-          currentElapstedTimeToConnect = millis();
-        }
-        break;
-    case startAdvertising:
-        connectionTimer = 0;
-        advertisingTimer = millis() + 10000; // advertise for a max of 10 seconds then give up
-        waitingTimer = 0;
-        updateCharacteristicValues();
-        BLE.advertise(&advData);
-        state = advertising;
-        break;
-    case advertising:
-        digitalWrite(D7, HIGH);
-        display.clearDisplay();
-        display.setCursor(0,0);
-        display.setTextSize(1);
-        display.print("   ...advertising...");
-        display.setCursor(30,18);
-        display.setTextSize(2);
-        display.print((millis() - currentElapstedTimeToConnect));
-        display.print(" ms");
-        display.display();
-        display.setTextSize(1);
-        if (advertisingTimerTriggered) {
-          state = cancelAdvertising;
-        }
-        break;
-    case cancelAdvertising:
-        digitalWrite(D7, LOW);
-        BLE.stopAdvertising();
-        connectionTimer = 0;
-        advertisingTimer = 0;
-        waitingTimer = millis() + (kWaitingTimerBetweenAdvertisements);
-        state = waiting;
-        break;
-    case connected:
-        display.clearDisplay();
-        display.setCursor(0,0);
-        display.print("connected in ...");
-        display.setCursor(30,18);
-        display.setTextSize(2);
-        display.print(finalElapsedTimeToConnect);
-        display.print(" ms");
-        display.display();
-        display.setTextSize(1);
-        if (connectionTimerTriggered) {
-          state = disconnect;
-        }
-        break;
-    case disconnect:
-        digitalWrite(D7, LOW);
-        BLE.disconnectAll();
-        BLE.stopAdvertising();
-        connectionTimer = 0;
-        advertisingTimer = 0;
-        waitingTimer = millis() + (kWaitingTimerBetweenAdvertisements);
-        // connectedTimer.stop();
-        // advertisingTimer.stop();
-        // waitingTimer.start();
-        state = waiting;
+  switch (state)
+  {
+  case waiting:
+    clearSeizure();
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print("A <- Advertise    ");
+    display.print((waitingTimer - millis()) / 1000);
+    display.println("s");
+    if (useAdvertisingPacketForSeizureAlert)
+    {
+      display.println("B <- Fast Mode: On");
+    }
+    else
+    {
+      display.println("B <- Fast Mode: Off");
+    }
+    display.println("C <- Seizure Detect!");
+    if (connectionCount > 0)
+    {
+      // display.drawLine(0,24,128,24,1);
+      display.setCursor(0, 25);
+      display.print(minTimeToConnect);
+      display.print(" - ");
+      display.print(totalFinalElapsedTimeToConnect / connectionCount);
+      display.print(" - ");
+      display.print(maxTimeToConnect);
+      display.print(" ");
+      display.println(connectionCount);
+    }
+    display.display();
+    if (waitingTimerTriggered)
+    {
+      state = startAdvertising;
+      currentElapstedTimeToConnect = millis();
+    }
+    break;
+  case startAdvertising:
+    connectionTimer = 0;
+    advertisingTimer = millis() + 10000; // advertise for a max of 10 seconds then give up
+    waitingTimer = 0;
+    updateCharacteristicValues();
+    BLE.advertise(&advData);
+    state = advertising;
+    break;
+  case advertising:
+    digitalWrite(D7, HIGH);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.print("   ...advertising...");
+    display.setCursor(30, 18);
+    display.setTextSize(2);
+    display.print((millis() - currentElapstedTimeToConnect));
+    display.print(" ms");
+    display.display();
+    display.setTextSize(1);
+    if (advertisingTimerTriggered)
+    {
+      state = cancelAdvertising;
+    }
+    break;
+  case cancelAdvertising:
+    digitalWrite(D7, LOW);
+    BLE.stopAdvertising();
+    connectionTimer = 0;
+    advertisingTimer = 0;
+    waitingTimer = millis() + (kWaitingTimerBetweenAdvertisements);
+    state = waiting;
+    break;
+  case connected:
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print("connected in ...");
+    display.setCursor(30, 18);
+    display.setTextSize(2);
+    display.print(finalElapsedTimeToConnect);
+    display.print(" ms");
+    display.display();
+    display.setTextSize(1);
+    if (connectionTimerTriggered)
+    {
+      state = disconnect;
+    }
+    break;
+  case disconnect:
+    digitalWrite(D7, LOW);
+    BLE.disconnectAll();
+    BLE.stopAdvertising();
+    connectionTimer = 0;
+    advertisingTimer = 0;
+    waitingTimer = millis() + (kWaitingTimerBetweenAdvertisements);
+    // connectedTimer.stop();
+    // advertisingTimer.stop();
+    // waitingTimer.start();
+    state = waiting;
   }
 }
 
-void setSeizure() {
-    seizureAlertCharacteristicUuid.setValue(1);
+void setSeizure()
+{
+  seizureAlertCharacteristicUuid.setValue(1);
 }
-void clearSeizure() {
-    seizureAlertCharacteristicUuid.setValue(0);
-    setAdvertisingManufacturingData(false);
-}
-
-void updateCharacteristicValues() {
-    insBatteryLevelCharacteristicUuid.setValue((int)insBattery);
-    insConnectionCharacteristicUuid.setValue(1);
-    ctmConnectionCharacteristicUuid.setValue(1);
-    tabletBatteryLevelCharacteristicUuid.setValue((int)tabletBattery);
-    tabletDiskSpaceCharacteristicUuid.setValue((int)tabletDiskSpace);
-    tabletConnectionCharacteristicUuid.setValue(1);
-
-    insBattery = insBattery - 0.2;
-    tabletBattery = tabletBattery - 0.1;
-    tabletDiskSpace = tabletDiskSpace - 0.05;
-
-    if(insBattery < 0) {
-      insBattery = 0;
-      resetBatteryLevels();
-    }
-    if(tabletBattery < 0) {
-      tabletBattery = 0;
-      resetBatteryLevels();
-    }
-    if(tabletDiskSpace < 0) {
-      tabletDiskSpace = 0;
-      resetBatteryLevels();
-    }
-}
-void resetBatteryLevels() {
-      insBattery = 100;
-      tabletBattery = 100;
-      tabletDiskSpace = 100;
+void clearSeizure()
+{
+  seizureAlertCharacteristicUuid.setValue(0);
+  setAdvertisingManufacturingData(false);
 }
 
-void handleTimedEvents() {
+void updateCharacteristicValues()
+{
+  insBatteryLevelCharacteristicUuid.setValue((int)insBattery);
+  insConnectionCharacteristicUuid.setValue(1);
+  ctmConnectionCharacteristicUuid.setValue(1);
+  tabletBatteryLevelCharacteristicUuid.setValue((int)tabletBattery);
+  tabletDiskSpaceCharacteristicUuid.setValue((int)tabletDiskSpace);
+  tabletConnectionCharacteristicUuid.setValue(1);
+
+  insBattery = insBattery - 0.2;
+  tabletBattery = tabletBattery - 0.1;
+  tabletDiskSpace = tabletDiskSpace - 0.05;
+
+  if (insBattery < 0)
+  {
+    insBattery = 0;
+    resetBatteryLevels();
+  }
+  if (tabletBattery < 0)
+  {
+    tabletBattery = 0;
+    resetBatteryLevels();
+  }
+  if (tabletDiskSpace < 0)
+  {
+    tabletDiskSpace = 0;
+    resetBatteryLevels();
+  }
+}
+void resetBatteryLevels()
+{
+  insBattery = 100;
+  tabletBattery = 100;
+  tabletDiskSpace = 100;
+}
+
+void handleTimedEvents()
+{
   connectionTimerTriggered = false;
   waitingTimerTriggered = false;
   advertisingTimerTriggered = false;
-  if (connectionTimer > 0) {
-    if (connectionTimer < millis()) {
+  if (connectionTimer > 0)
+  {
+    if (connectionTimer < millis())
+    {
       connectionTimerTriggered = true;
       connectionTimer = 0;
     }
   }
-  if (advertisingTimer > 0) {
-    if (advertisingTimer < millis()) {
+  if (advertisingTimer > 0)
+  {
+    if (advertisingTimer < millis())
+    {
       advertisingTimerTriggered = true;
       advertisingTimer = 0;
     }
   }
-  if (waitingTimer > 0) {
-    if (waitingTimer < millis()) {
+  if (waitingTimer > 0)
+  {
+    if (waitingTimer < millis())
+    {
       waitingTimerTriggered = true;
       waitingTimer = 0;
     }
   }
+}
+uint8_t getPacketNumber() {
+  return 1;
 }
